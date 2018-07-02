027cb3c4e62b092ec58f5b352aa1c55565e2a3d9
USER-ERROR-42 <32377819+USER-ERROR-42@users.noreply.github.com>
2018-07-02 12:36:41 -0700

    SQL statements updated to work with MS SQL Server

---
+++ KaibaReduxAPI/Controllers/DbAccessManagement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KaibaReduxAPI.Controllers
+{
+    public class DbAccessManagement
+    {
+    }
+}
