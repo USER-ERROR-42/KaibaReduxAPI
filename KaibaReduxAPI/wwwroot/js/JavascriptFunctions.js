86a7d4865eafa684d637433d4c96fdfa821af6fd
USER-ERROR-42 <32377819+USER-ERROR-42@users.noreply.github.com>
2018-07-15 23:22:22 -0700

    edit item functionality completely implemented

--- KaibaReduxAPI/wwwroot/js/JavascriptFunctions.js
+++ KaibaReduxAPI/wwwroot/js/JavascriptFunctions.js
@@ -32,13 +32,17 @@ function getMenus() {
             for (let i = 0; i < menuList.length; i++) {
                 let menu = menuList[i]; 
 
+                // add line break if not the first menu
+                if (i > 0) {
+                    $('<br />').appendTo($(MENUS_LIST_DIV_ID));
+                }
+
                 // Using Jquery create a new <p> object with the menu's info
                 // It has id property: id = menu#, where # is that menu's id number
                 // It also adds an onclick event, so that when clicked it will call showMenu(#), where # is that menu's id number
                 $('<button id="menu' + menu.id + '" onclick="showMenu(' + menu.id + ')"><strong> Show ' + menu.name + '</strong></button>' +
                     '<button class="edit" onclick="editMenu(' + menu.id + ')"> Edit </button>' + // adds a button to go to the menu edit page
-                    '<button id="deleteMenu' + menu.id + '" class="delete" onclick="deleteMenu(' + menu.id + ')"> Delete </button>' + // adds a button to go to delete the menu
-                    '<br />'
+                    '<button id="deleteMenu' + menu.id + '" class="delete" onclick="deleteMenu(' + menu.id + ')"> Delete </button>' // adds a button to delete the menu
                 ).appendTo($(MENUS_LIST_DIV_ID)); // Add it to the menuList div
             }
 
@@ -113,27 +117,31 @@ function showSection(section) {
     $('<p id="section' + section.id + '" ><strong>' + section.name + '</strong></p>' +
         '<p><i> ' + section.description + ' </i></p>' +
         '<button class="edit" onclick="editSection(' + section.id + ')"> Edit ' + section.name + '</button>' + // adds a button to go to the section edit page
-        '<button id="deleteSection' + section.id + '" class="delete" onclick="deleteSection(' + section.id + ')"> Delete ' + section.name + '</button>' + // adds a button to go to delete the section
-        '<br /> <br />'
+        '<button id="deleteSection' + section.id + '" class="delete" onclick="deleteSection(' + section.id + ')"> Delete ' + section.name + '</button>' + // adds a button to delete the section
+        '<br />'
     ).appendTo($(MENU_CONTENTS_DIV_ID)); // Add it to the specified div
+
+    // add a create new item button
+    $('<button class="edit" onclick="editItem(-' + section.id + ', ' + section.menuID + ')"> Create new Item in ' + section.name + '</button> <br /> <br />').appendTo($(MENU_CONTENTS_DIV_ID));
 
     // loop through the itemList variable of the section object
     for (let i = 0; i < section.itemList.length; i++) {
         let item = section.itemList[i];
 
         // display each item 
-        showItem(item);
+        showItem(item, section.menuID);
     }
-
-    // add line break
-    $('<br />').appendTo($(MENU_CONTENTS_DIV_ID));
+    
 }
 
-function showItem(item) {
+function showItem(item, menuID = 1) {
     // takes an item object and adds <p> describing it to the menu contents div (given by the constant MENU_CONTENTS_DIV_ID)
+    // second parameter is the menuID of the section that this menu is in, which defaults to 1 if not specified
 
     $('<p id="item' + item.id + '" >' + item.position + '. ' + item.name + '</p>' +
-        '<p><i> ' + item.description + ' </i></p>'
+        '<p><i> ' + item.description + ' </i></p>' +
+        '<button class="edit" onclick="editItem(' + item.id + ', ' + menuID + ')"> Edit ' + item.name + '</button>' + // adds a button to go to the item edit page
+        '<button id="deleteItem' + item.id + '" class="delete" onclick="deleteItem(' + item.id + ')"> Delete ' + item.name + '</button>' // adds a button to delete the item
     ).appendTo($(MENU_CONTENTS_DIV_ID)); // Add it to the specified div
 
     // check if there's only one priceline item with no description
@@ -254,7 +262,7 @@ function submitMenu(create) {
         },
         success: function (result) {
             alert('Menu successfully ' + operationString);
-            window.location = "index.html";
+            window.location = "index.html?id=" + menu.id;
             
         }
     });
@@ -300,7 +308,7 @@ function deleteMenu(id) {
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 // Change button text to show delete failure
-                $(buttonID).text("Delete Failed");
+                $(buttonID).text("Delete Failed. There are still sections in this menu");
             }
         });
     }
@@ -362,7 +370,7 @@ function submitSection(create) {
         methodString = "PUT";
         operationString = "updated";
     }
-    console.log(JSON.stringify(section));
+    //console.log(JSON.stringify(section));
 
     // ajax call
     $.ajax({
@@ -376,7 +384,7 @@ function submitSection(create) {
         },
         success: function (result) {
             alert('Section successfully ' + operationString);
-            window.location = "index.html";
+            window.location = "index.html?id=" + section.menuID;
 
         }
     });
@@ -388,10 +396,131 @@ function deleteSection(id) {
     // second call sends an ajax request to delete that entry, then gets the sections again
 
     // the confirm delete text
-    let confirmDeleteText = "Confirm Delete?";
+    let confirmDeleteText = "Confirm Delete, including all items in this section?";
 
     // id of current button
     let buttonID = "#deleteSection" + id;
+
+    if ($(buttonID).text() != confirmDeleteText) {
+        // first call changes the text of the calling button
+        $(buttonID).text(confirmDeleteText)
+    }
+    else {
+        // second call sends the ajax delete request
+
+        // JSON to be sent, only requires the id
+        let jsonString = '{ "id": ' + id + ' }';
+
+        $.ajax({
+            method: "DELETE",                     // method is DELETE
+            accepts: 'application/json',
+            url: URL + "section",                  // url is api/section
+            contentType: 'application/json',    // type of data being sent
+            data: jsonString,                   // actual data being sent, use JSON library to convert the section object to JSON
+            success: function (result) {
+                // Change button text
+                $(buttonID).text("Successfully Deleted");
+
+                // Remove this button's onclick event
+                $(buttonID).prop('onclick', null).off('click');
+
+                // refresh current menu
+                showMenu(currentMenuID);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                // Change button text to show delete failure
+                $(buttonID).text("Delete Failed");
+            }
+        });
+    }
+}
+
+function editItem(itemID, menuID) {
+    // redirects to the Item Edit/Create page,
+    // first parameter is either a positive item or a negative section id
+    // (a negative number signifies that this is a create operation)
+    // second parameter is a menuID
+
+    let param = "?id=" + itemID + "&menuID=" + menuID;
+    window.location = "editItem.html" + param;
+}
+
+function loadItem(id) {
+    // takes an item id and loads it's info into the input elements (id, name, description, and position)
+
+    // Jquery ajax call to get the specific item
+    $.ajax({
+        method: 'GET',
+        url: URL + "item/" + id,    // The URL we want is api/item/#
+        dataType: "json",
+        success: function (item) {  // function called if successful
+            $('#id').val(item.id);
+            $('#name').val(item.name);
+            $('#description').val(item.description);
+            $('#position').val(item.position);
+            $('#sectionID').val(item.sectionID);
+            $('#picturePath').val(item.picturePath);
+        },
+        error: function (jqXHR, status, errorThrown) {      // This function will run if there's an error
+            alert("ERROR: Can't retrieve this item " + errorThrown + " ");   // Pop up a textbox with an error message
+        }
+    });
+}
+
+function submitItem(create, menuID = 1) {
+    // takes the values of the item input elements (id, name, description, and position)
+    // and passes them to the api using create if true is passed and using edit if false is passed
+    // second parameter is the menuID, used for the direct back to the display menu page (defaults to 1)
+
+    // create item object by getting value attribute of the input elements
+    let item = {
+        "id": parseInt($('#id').val()),
+        "name": $('#name').val(),
+        "description": $('#description').val(),
+        "position": parseFloat($('#position').val()),
+        "sectionID": $('#sectionID').val(),
+        "picturePath": $('#picturePath').val()
+    };
+
+    // request method is either POST (create) or PUT (edit)
+    let methodString, operationString;
+    if (create) {
+        methodString = "POST";
+        operationString = "created";
+    }
+    else {
+        methodString = "PUT";
+        operationString = "updated";
+    }
+    console.log(JSON.stringify(item));
+
+    // ajax call
+    $.ajax({
+        method: methodString,               // either POST or PUT
+        accepts: 'application/json',
+        url: URL + "item",                  // url is api/item
+        contentType: 'application/json',    // type of data being sent
+        data: JSON.stringify(item),         // actual data being sent, use JSON library to convert the item object to JSON
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('Error: Item could not be ' + operationString);
+        },
+        success: function (result) {
+            alert('Item successfully ' + operationString);
+            window.location = "index.html?id=" + menuID;
+        }
+    });
+}
+
+function deleteItem(id) {
+    // called by a delete button
+    // first call changes the button's text to "Confirm Delete?"
+    // second call sends an ajax request to delete that entry, then gets the items again
+
+    // the confirm delete text
+    let confirmDeleteText = "Confirm Delete, including all this item's prices?";
+
+    // id of current button
+    let buttonID = "#deleteItem" + id;
 
     if ($(buttonID).text() != confirmDeleteText) {
         // first call changes the text of the calling button
@@ -407,9 +536,9 @@ function deleteSection(id) {
         $.ajax({
             method: "DELETE",                     // method is DELETE
             accepts: 'application/json',
-            url: URL + "section",                  // url is api/section
+            url: URL + "item",                  // url is api/item
             contentType: 'application/json',    // type of data being sent
-            data: jsonString,                   // actual data being sent, use JSON library to convert the section object to JSON
+            data: jsonString,                   // actual data being sent, use JSON library to convert the item object to JSON
             success: function (result) {
                 // Change button text
                 $(buttonID).text("Successfully Deleted");
